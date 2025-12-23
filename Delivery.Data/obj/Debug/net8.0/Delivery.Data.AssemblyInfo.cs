5e8d5aa7da8505ae4322b424e260b358479457da
student <student@541474mb.local>
2025-12-23 15:46:24 +0200

    Add my current code

--- Delivery.Data/obj/Debug/net8.0/Delivery.Data.AssemblyInfo.cs
+++ Delivery.Data/obj/Debug/net8.0/Delivery.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delivery.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dace6aea0acecbc43b0f30667fa1dd6c52a177")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delivery.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delivery.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
